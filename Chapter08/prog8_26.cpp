f34a264efa9094902bb4488bddd48584b521c089
justrockaoi <88675605+justrockaoi@users.noreply.github.com>
2022-01-24 15:25:41 +0800

    Add files via upload

--- Chapter08/prog8_26.cpp
+++ Chapter08/prog8_26.cpp
@@ -10,7 +10,9 @@ int main(void)
 					 {77,33,68,45,23},
 					 {43,55,43,67,65}};
 	int saler[3];
-	int max=0;
+	int Psale[5];
+	int max=0,s=0;
+	int Pmax=0, Ps=0;
 	
 	cout << "1.Calculate Each Saler Total" << endl;				 
 	for(int i=0;i<3;i++) 
@@ -20,20 +22,23 @@ int main(void)
 		for(int j=0 ;j<5; j++)
 		{
 			total = PDT[j]*sales[i][j];	
-			sum +=total;		
+			sum +=total;
+					
 		}
-		
+			
 		saler[i]=sum;
 		if(saler[i]>max)
-			max = saler[i];
-			
-		cout <<sum << endl;	
+			{
+				max = saler[i];
+				s=i;
+			}
+		cout <<sum << endl;
+		sum=0;	
 	}
-	sum=0;
+	
 	cout << endl;
 	
 	////////////////////////////////////////////////////////
-	
 	
 	cout << "2.Each Product Total" << endl;
 	
@@ -44,15 +49,31 @@ int main(void)
 		for(int i =0 ;i<3; i++)
 		{
 			total = PDT[j]*sales[i][j];
-			sum +=total;	
+			sum +=total;
 		}	
 		cout <<sum << endl;	
+		
+		Psale[j]=sum;
+		if(Pmax<Psale[j])
+			{
+				Pmax=Psale[j];
+			    Ps=j;
+			}
 	}
 	cout << endl;
+	
 	//////////////////////////////////////////////////////////
 	
+	cout << "The most sale is num "<< s <<endl;
+	cout << "Total is "<< max <<endl;
+	cout << endl;
 	
-	cout << max <<endl;
+	//////////////////////////////////////////////////////////
+	
+	cout << "The most sale Product "<< Ps <<endl;
+	cout << "Total is "<< Pmax <<endl;
+	cout << endl;
+	
 	
 	system("pause");
 	return 0;

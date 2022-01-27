e20c91a7f1a7b9d3769fcd36fbad9ad2108adb04
justrockaoi <88675605+justrockaoi@users.noreply.github.com>
2022-01-27 14:43:34 +0800

    Update prog8_46.cpp

--- Chapter08/prog8_46.cpp
+++ Chapter08/prog8_46.cpp
@@ -6,8 +6,8 @@ using namespace std;
 int main(void)
 {
 	int num;
-	char month[12][15]={"January","February","March" ,"April","May","June",
-				"July","August","September","November","December"};
+	string month[12]={"January","February","March" ,"April","May","June",
+			"July","August","September","November","December"};
 	cout << "Enter the number of month:";
 	cin >>num;
 	

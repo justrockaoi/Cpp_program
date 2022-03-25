b7b9dc3268bc5aaa1ab96599f33c2e95648d9e0a
justrockaoi <88675605+justrockaoi@users.noreply.github.com>
2022-03-25 14:29:20 +0800

    Add files via upload

---
+++ Chapter10/prog10_16.cpp
@@ -0,0 +1,24 @@
+#include <iostream>
+#include <cstdlib>
+using namespace std;
+
+int min(int &,int &);
+
+int main(void)
+{
+	int a=32,b=59;
+	cout << "a=" << a << ", b=" << b << endl;
+	min(a,b);
+	cout << "a=" << a << ", b=" << b << endl;
+	
+	return 0;
+}
+
+int min(int &x,int &y)
+{
+	if(x>y)
+		return y=100;
+	else
+		return x=100;
+}
+
